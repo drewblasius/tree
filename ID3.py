fd4d9fdfc7f5bd7afb654b1216725b6e788acb57
Drew Blasius <andrew.blasius@case.edu>
2016-10-10 14:27:53 -0400

    finished ID3Tree.ID3()

--- ID3.py
+++ ID3.py
@@ -1,35 +1,120 @@
+## Docstring stuff
+
+
 
 class ID3Tree(object):
-    def __init__(self):
-        pass
+    def __init__(self,trainingData=None,attributeValues=None):
+        self.setTrainingData(trainingData)
+        self.setAttributeValues(attributeValues)
 
+    # Setter and getter methods -- 
     def setRoot(self,root):
+        # sets root of tree
         self.__root = root
 
     def getRoot(self):
+        # gets root of tree
         return self.__root
 
-    def findByAttribute(examples,attribute,value):
-        # examples is a 2 x N numpy array which looks like
-        # [[ex1,val1],
-        #  [ex2,val2],
-        #  [exN,valN]],
-        # where exJ are themselves 1-d arrays of length k.
-        # returns a subarray with examples[attribute] = value
-        
+    def setTrainingData(self,trainingData):
+        # trainingData should be a (k + 1) x N - dimensional numpy array
+        # which looks like
+        # [[ex1 val1],
+        #  [ex2 val2],
+        #  ..........
+        #  [exN valN]]
+        # where each example has k attributes
+        self.__trainingData = trainingData
 
+    def getTrainingData(self):
+        # gets training data (see setTrainingData for formatting)
+        return self.__trainingData
 
+    def setAttributeValues(self,attributeValues):
+        # sets attribute values
+        self.__attributeValues = attributeValues
+
+    def getAttributeValues(self):
+        # gets attribute values
+        return self.__attributeValues
+    # end of setter/getter methods --
+
+    #TBD
+    def getBestAttribute(self,examples):
+        # given a set of examples, gets the best attribute (see find for 
+        # examples formatting). Does so by computing the infomation gain of 
+        # each attribute. 
+        pass
 
     def split(node,examples):
         bestAttribute = self.getBestAttribute(examples)
-        node.setAttrbute(bestAttribute)
+        node.setAttribute(bestAttribute)
         if self.getAttributeValues() is None:
             raise ValueError("Attribute Values must be set prior to training!")
         else:
             for value in self.getAttributeValues()[bestAttribute]:
-                child = node.addChild()
-                #Get the training examples which have this value
-                node.addData(correctExamples) # needs to be finished
-                if not isPure(child.getData()):
-                    self.split(child,correctExamples)
+                # add a new node
+                child = Node()
+                node.addChild(child) 
+                # add examples   
+                nextExamples = examples[ np.where(examples[:,bestAttribute] == value) ]
+                if not child.isPure():
+                    self.split(child,nextExamples)
+
+    def ID3(self):
+        if self.getTrainingData() is None:
+            raise ValueError("Tree needs training data!")
+        elif self.getAttributeValues() is None:
+            raise ValueError("Tree needs attribute values!")
+        elif self.getRoot() is not None:
+            raise ValueError("Root of tree is already set!")
+        else:
+            root = Node()
+            self.setRoot(root)
+            self.split(root,self.getTrainingData())
+
+    #TBD
+    def classify(self,instance): 
+        if self.getRoot() is None:
+            raise ValueError("Tree has not been initialized.")
+        else:
+            currentNode = self.getRoot()
+
+
+class Node(object):
+    def __init__(self):
+        pass
+
+    def setData(self,data):
+        self.__data = data
+
+    def getData(self):
+        return self.__data
+
+    def setChildren(self,children):
+        self.__children = children
+
+    def getChildren(self):
+        return self.__children
+
+    def setAttribute(self,attribute):
+        self.__attribute = attribute
+
+    def getAttribute(self):
+        return self.__attribute
+
+    # not sure if the set/get parent methods are necessary
+    def setParent(self,parent):
+        self.__parent = parent
+
+    def getParent(self):
+        return self.__parent
+
+    def addChild(self,child):
+        self.getChildren().append(child)
+    
+    #TBD
+    def isPure(self):
+        pass
+
 
